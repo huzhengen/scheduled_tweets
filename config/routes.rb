65359a5d72a1e8a8d5714e1fc674bd1365283f3b
huzhengen <huzhengen@163.com>
2021-12-21 15:43:02 +0800

    Registration

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get '/about', to: 'about#index'
+  get '/sign_up', to: 'registrations#new'
+  post '/sign_up', to: 'registrations#create'
   # get '/', to: 'main#index'
   root to: 'main#index'
 end
